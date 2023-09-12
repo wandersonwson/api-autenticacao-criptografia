c6b233a5c8ef1085d362ce1f371b99973f3c0085
wandersonwson <wandersonwson@hotmail.com>
2023-09-12 16:10:14 -0300

    Geração do token

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,5 +1,7 @@
 import pool from "../dados/conexao.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import chave from "../autenticacao/chaveJWT.js";
 async function listarUsuarios(request, response) {
     try {
         const { rows } = await pool.query("select id, nome, email from usuarios");
@@ -35,17 +37,19 @@ async function detalharUsuario(request, response) {
     }
 }
 async function login(request, response) {
-    const { email,senha } = request.body;
+    const { email, senha } = request.body;
     try {
-        const usuario = await pool.query("select * from usuarios where email = $1", [email]);
-        if(usuario.rowCount < 1) {
+        const { rows, rowCount } = await pool.query("select * from usuarios where email = $1", [email]);
+        if(rowCount < 1) {
             return response.status(400).json({mensagem: "Email ou senha inválidos"});
         }
-        const senhaValida = await bcrypt.compare(senha, usuario.rows[0].senha);
+        const senhaValida = await bcrypt.compare(senha, rows[0].senha);
         if(!senhaValida) {
             return response.status(400).json({mensagem: "Email ou senha inválidos"});
         }
-        return response.json({mensagem: "Usuário logado"});
+        const token = jwt.sign({id: rows[0].id}, chave, {expiresIn: "8h"});
+        const {senha: cifra, ...usuario} = rows[0];
+        return response.json({usuario: usuario, token});
     } catch (error) {
         response.status(500).json({mensagem: "Erro interno no servidor"});
     }
